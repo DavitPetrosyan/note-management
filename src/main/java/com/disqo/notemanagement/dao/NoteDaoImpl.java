994f0a510c8283496cb603b704cf88990de4757a
David Petrosyan <david.petrosyan@synisys.com>
2019-05-20 15:13:12 +0400

    Added DUMMY dao layer for notes

---
+++ src/main/java/com/disqo/notemanagement/dao/NoteDaoImpl.java
@@ -0,0 +1,42 @@
+package com.disqo.notemanagement.dao;
+
+import com.disqo.notemanagement.model.NoteDto;
+
+import java.util.Collection;
+import java.util.Optional;
+
+/**
+ * author by davitpetrosyan on 2019-05-20.
+ */
+public class NoteDaoImpl implements NoteDao {
+
+	@Override
+	public Collection<NoteDto> getAllNotes() {
+		return null;
+	}
+
+	@Override
+	public Collection<NoteDto> getUserAllNotes(Integer userId) {
+		return null;
+	}
+
+	@Override
+	public Optional<NoteDto> getNoteById(Integer noteId) {
+		return Optional.empty();
+	}
+
+	@Override
+	public Integer createNote(NoteDto noteDto) {
+		return null;
+	}
+
+	@Override
+	public void updateNote(NoteDto noteDto) {
+
+	}
+
+	@Override
+	public void deleteNote(Integer noteId) {
+
+	}
+}
